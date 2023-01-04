df288c426161d882ac9321711d447bbce4acf6ff
slawinangsan <118531114+slawinangsan@users.noreply.github.com>
2023-01-04 21:23:59 +0100

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,22 @@
 
+$(document).ready(function(){
+	$(".fa-bars").click(function(){
+		$(".menu").animate({left:"270px"},600);
+		$(".fa-bars").animate({left:"270px"},600);
+		$(".text_1").animate({left:"770px"},600);
+		$(".fa-close").css("visibility" , "visible");
+		$(".fa-close").animate({left:"270px"},600);
+		$(".fa-bars").css("visibility" , "hidden");
+	});
+});
+
+$(document).ready(function(){
+	$(".fa-close").click(function(){
+		$(".menu").animate({left:"-270px"},700);
+		$(".fa-bars").animate({left:"0px"},600);
+		$(".text_1").animate({left:"685px"},600);
+		$(".fa-close").css("visibility" , "hidden");
+		$(".fa-close").animate({left:"0px"},600);
+		$(".fa-bars").css("visibility" , "visible");
+	});
+});
